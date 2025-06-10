b5dc7bcbe9498f44e542ed797ec492df11bea771
islamAbukoush <islam.abukoush@isa.utm.md>
2025-06-10 22:35:38 +0300

    modified a bit

--- app/[locale]/(main)/_components/Bitrix24Form.tsx
+++ app/[locale]/(main)/_components/Bitrix24Form.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-export const Bitrix24Form = ({ src, data }: { src: string; data: string }) => {
+export const Bitrix24Form = ({ src, data, className }: { src: string; data: string, className?: string }) => {
   useEffect(() => {
     const script = document.createElement('script');
     script.src = src + '?' + ((Date.now() / 180000) | 0);
@@ -10,5 +10,5 @@ export const Bitrix24Form = ({ src, data }: { src: string; data: string }) => {
       document.body.removeChild(script);
     };
   }, [src]);
-  return <script data-b24-form={data} data-skip-moving='true'></script>;
+  return <script data-b24-form={data} data-skip-moving='true' className={`${className}`}></script>;
 };
--- app/[locale]/(main)/_components/RuForm.tsx
+++ app/[locale]/(main)/_components/RuForm.tsx
@@ -1,9 +1,10 @@
 "use client"
 import { Bitrix24Form } from "./Bitrix24Form";
 
-export default function RuForm() {
+export default function RuForm({ className }: {className?: string}) {
   return (
     <Bitrix24Form
+      className={`${className}`}
       src='https://cdn-ru.bitrix24.ru/b31886438/crm/form/loader_16.js'
       data='inline/16/dhirfh'
     />
