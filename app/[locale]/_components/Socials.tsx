45613af6441f7c2f627bcd5753c8a3b3d78704a7
islamAbukoush <islam.abukoush@isa.utm.md>
2025-06-13 22:33:16 +0300

    changed telegram position

--- app/[locale]/_components/Socials.tsx
+++ app/[locale]/_components/Socials.tsx
@@ -3,6 +3,11 @@ import Link from "next/link"
 export default function Socials() {
     const socials = [
         {
+            link: "https://t.me/+37360410999",
+            image: "/svg/telegram.svg",
+            name: "telegram"
+        },
+        {
             link: "https://www.facebook.com/perfectgarage.md/",
             image: "/svg/facebook.svg",
             name: "facebook"
@@ -17,14 +22,10 @@ export default function Socials() {
             image: "/svg/whatsapp.svg",
             name: "whatsapp"
         },
-        {
-            link: "https://t.me/+37360410999",
-            image: "/svg/telegram.svg",
-            name: "telegram"
-        }
+
     ]
     return (<div className="flex flex-col gap-2 fixed right-[58px] bottom-[120px]">
-        {socials.map(({link, image, name}, i) => (<Link key={i} href={link} className="cursor-pointer hover:scale-120 transition-transform" target="blank">
+        {socials.map(({ link, image, name }, i) => (<Link key={i} href={link} className="cursor-pointer hover:scale-120 transition-transform" target="blank">
             <Image src={image} alt={`${name} icon`} width={50} height={50} />
         </Link>))}
     </div>)
