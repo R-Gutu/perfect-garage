e6e37c383adae808caeb71f987cd2b8e47c9efba
islamAbukoush <islam.abukoush@isa.utm.md>
2025-06-13 22:32:20 +0300

    added telegram

--- app/[locale]/_components/Socials.tsx
+++ app/[locale]/_components/Socials.tsx
@@ -16,10 +16,15 @@ export default function Socials() {
             link: "https://wa.me/37360410999",
             image: "/svg/whatsapp.svg",
             name: "whatsapp"
+        },
+        {
+            link: "https://t.me/+37360410999",
+            image: "/svg/telegram.svg",
+            name: "telegram"
         }
     ]
     return (<div className="flex flex-col gap-2 fixed right-[58px] bottom-[120px]">
-        {socials.map(({link, image, name}, i) => (<Link href={link} className="cursor-pointer hover:scale-120 transition-transform" target="blank">
+        {socials.map(({link, image, name}, i) => (<Link key={i} href={link} className="cursor-pointer hover:scale-120 transition-transform" target="blank">
             <Image src={image} alt={`${name} icon`} width={50} height={50} />
         </Link>))}
     </div>)
