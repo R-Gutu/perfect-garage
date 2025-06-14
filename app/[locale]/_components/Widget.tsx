d428048748d1e603d564f5c41ab355b7804df153
islamAbukoush <islam.abukoush@isa.utm.md>
2025-06-14 16:00:06 +0300

    widget

--- app/[locale]/_components/Widget.tsx
+++ app/[locale]/_components/Widget.tsx
@@ -7,9 +7,9 @@ export default function Widget() {
       dangerouslySetInnerHTML={{
         __html: `
           (function(w,d,u){
-            var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
-            var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-          })(window,document,'https://cdn-ru.bitrix24.ru/b31886438/crm/site_button/loader_4_hsk47i.js');
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://cdn-ru.bitrix24.ru/b31886438/crm/site_button/loader_4_hsk47i.js');
         `
       }}
     />
