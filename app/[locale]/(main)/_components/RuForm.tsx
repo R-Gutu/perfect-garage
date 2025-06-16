a4c7893f68f4e5bccdba10929397f6717fc5c886
islamAbukoush <islam.abukoush@isa.utm.md>
2025-06-16 17:23:24 +0300

    Update Bitrix24 form scripts and enable Widget component in layout

--- app/[locale]/(main)/_components/RuForm.tsx
+++ app/[locale]/(main)/_components/RuForm.tsx
@@ -5,8 +5,8 @@ export default function RoForm({ className }: { className?: string }) {
   return (
     <Bitrix24Form
       className={`${className}`}
-      src='https://cdn-ru.bitrix24.ru/b31886438/crm/form/loader_16.js'
-      data='inline/16/dhirfh'
+      src='https://cdn-ru.bitrix24.ru/b31886438/crm/form/loader_34.js'
+      data='inline/34/jx6uo4'
     />
   );
 }
--- app/[locale]/_components/Widget.tsx
+++ app/[locale]/_components/Widget.tsx
@@ -1,17 +1,34 @@
-import Script from "next/script";
+'use client'
+import { useLocale } from "next-intl";
+import { useEffect } from "react";
 
 export default function Widget() {
-  return (
-    <Script
-      id="bitrix-loader"
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function(w,d,u){
-                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
-                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-        })(window,document,'https://cdn-ru.bitrix24.ru/b31886438/crm/site_button/loader_4_hsk47i.js');
-        `
-      }}
-    />
-  );
+  const locale = useLocale();
+
+  useEffect(() => {
+    // Delete the Bitrix24 script on locale change
+    const existingScript = document.querySelector(':has(> .b24-widget-button-shadow)');
+    if (existingScript) {
+      existingScript.remove();
+    }
+    if (locale === 'ro') {
+      (function (w, d, u) {
+        var s = d.createElement('script'); s.async = true; s.src = u + '?' + (Date.now() / 60000 | 0);
+        var h = d.getElementsByTagName('script')[0];
+        if (h && h.parentNode) {
+          h.parentNode.insertBefore(s, h);
+        }
+      })(window, document, 'https://cdn-ru.bitrix24.ru/b31886438/crm/site_button/loader_8_0nu8yh.js');
+    } else {
+      (function (w, d, u) {
+        var s = d.createElement('script'); s.async = true; s.src = u + '?' + (Date.now() / 60000 | 0);
+        var h = d.getElementsByTagName('script')[0];
+        if (h && h.parentNode) {
+          h.parentNode.insertBefore(s, h);
+        }
+      })(window, document, 'https://cdn-ru.bitrix24.ru/b31886438/crm/site_button/loader_4_hsk47i.js');
+    }
+  }, [locale]);
+
+  return(<></>)
 }
