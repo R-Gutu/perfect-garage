40a079b7eb50341ae4507a668f271d30c770691e
islamAbukoush <islam.abukoush@isa.utm.md>
2025-06-16 17:29:35 +0300

    Refactor Widget component to streamline script loading based on locale

--- app/[locale]/_components/Widget.tsx
+++ app/[locale]/_components/Widget.tsx
@@ -3,32 +3,24 @@ import { useLocale } from "next-intl";
 import { useEffect } from "react";
 
 export default function Widget() {
-  const locale = useLocale();
+  const locale = useLocale() as 'ro' | 'ru';
 
   useEffect(() => {
+    const scripts: Record<'ro' | 'ru', string> = {
+      'ro': 'https://cdn-ru.bitrix24.ru/b31886438/crm/site_button/loader_8_0nu8yh.js',
+      'ru': 'https://cdn-ru.bitrix24.ru/b31886438/crm/site_button/loader_4_hsk47i.js'
+    };
     // Delete the Bitrix24 script on locale change
     const existingScript = document.querySelector(':has(> .b24-widget-button-shadow)');
     if (existingScript) {
       existingScript.remove();
     }
-    if (locale === 'ro') {
-      (function (w, d, u) {
-        var s = d.createElement('script'); s.async = true; s.src = u + '?' + (Date.now() / 60000 | 0);
-        var h = d.getElementsByTagName('script')[0];
-        if (h && h.parentNode) {
-          h.parentNode.insertBefore(s, h);
-        }
-      })(window, document, 'https://cdn-ru.bitrix24.ru/b31886438/crm/site_button/loader_8_0nu8yh.js');
-    } else {
-      (function (w, d, u) {
-        var s = d.createElement('script'); s.async = true; s.src = u + '?' + (Date.now() / 60000 | 0);
-        var h = d.getElementsByTagName('script')[0];
-        if (h && h.parentNode) {
-          h.parentNode.insertBefore(s, h);
-        }
-      })(window, document, 'https://cdn-ru.bitrix24.ru/b31886438/crm/site_button/loader_4_hsk47i.js');
+    const s = document.createElement('script'); s.async = true; s.src = scripts[locale] + '?' + (Date.now() / 60000 | 0);
+    const h = document.getElementsByTagName('script')[0];
+    if (h && h.parentNode) {
+      h.parentNode.insertBefore(s, h);
     }
   }, [locale]);
 
-  return(<></>)
+  return (<></>)
 }
