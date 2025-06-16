5c675bc058889ef710aecf4590cb6340f80ca5a8
islamAbukoush <islam.abukoush@isa.utm.md>
2025-06-16 17:39:41 +0300

    Update Modal component to limit maximum height and adjust Bitrix24 form sources for localization

--- app/[locale]/(main)/_components/RoForm.tsx
+++ app/[locale]/(main)/_components/RoForm.tsx
@@ -5,8 +5,8 @@ export default function RuForm({ className }: { className?: string }) {
   return (
     <Bitrix24Form
       className={`${className}`}
-      src='https://cdn-ru.bitrix24.ru/b31886438/crm/form/loader_36.js'
-      data='inline/36/b89mzl'
+      src='https://cdn-ru.bitrix24.ru/b31886438/crm/form/loader_18.js'
+      data='inline/18/t5y7w3'
     />
   );
 }
