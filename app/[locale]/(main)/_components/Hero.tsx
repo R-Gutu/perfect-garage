970f3794e00285b3fa7afa30c5697d91b2887888
islamAbukoush <islam.abukoush@isa.utm.md>
2025-06-17 12:04:27 +0300

    Refactor Hero component button structure for improved semantics and accessibility

--- app/[locale]/(main)/_components/Hero.tsx
+++ app/[locale]/(main)/_components/Hero.tsx
@@ -46,12 +46,10 @@ const Hero = ({ setModalOpen }: { setModalOpen: (bool: boolean) => void }) => {
                 <MdKeyboardArrowRight className="group-hover:translate-x-1 transition-transform duration-300" size={20} />
               </button>
 
-              <button className="w-full sm:w-auto flex items-center justify-center sm:justify-start cursor-pointer bg-transparent border border-gray-400 hover:bg-white hover:text-gray-800 transition-all duration-300 rounded-xl sm:rounded-2xl font-semibold text-white text-sm sm:text-base hover:scale-105 ">
-                <Link href="tel:+373(60)410999" className="font-manrope w-full h-full flex items-center justify-center gap-3 px-6 sm:px-8 py-3 sm:py-4 ">
+              <Link href="tel:+373(60)410999" className="w-full sm:w-auto flex items-center justify-center sm:justify-start cursor-pointer bg-transparent border border-gray-400 hover:bg-white hover:text-gray-800 transition-all duration-300 rounded-xl sm:rounded-2xl font-semibold text-white text-sm sm:text-base hover:scale-105 font-manrope h-full gap-3 px-6 sm:px-8 py-3 sm:py-4 ">
                   <BsTelephone size={16} />
                   {t("consultBtn")}
-                </Link>
-              </button>
+              </Link>
             </div>
           </div>
         </div>
